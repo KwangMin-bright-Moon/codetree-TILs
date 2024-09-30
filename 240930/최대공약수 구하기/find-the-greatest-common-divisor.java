986c5738c612d3196cb18377b935c879fbde1e16
문광민 <113031724+KwangMin-bright-Moon@users.noreply.github.com>
2024-09-30 23:56:58 +0900

    Title: 최대공약수 구하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240930/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        findGcd(n,m);
+    }
+
+    private static void findGcd(int n, int m){
+       int gcd = 0;
+       for(int i = 1; i <= Math.min(n, m); i++){
+          if(n % i == 0 && m % i == 0){
+            gcd = i;
+          }
+       }
+
+       System.out.println(gcd);
+    }
+}
