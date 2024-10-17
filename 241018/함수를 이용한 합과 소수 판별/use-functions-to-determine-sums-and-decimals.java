a0917cd2983df0cfc2a50dc36add368a5f5fc306
문광민 <113031724+KwangMin-bright-Moon@users.noreply.github.com>
2024-10-18 07:42:47 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241018/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int cnt = 0;
+
+        for(int i = a; i < b + 1; i++){
+            if(isPrime(i) && isSumEven(i)){
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    private static boolean isPrime(int num){
+        if(num == 1){
+            return false;
+        }
+
+        if(num == 2){
+            return true;
+        }
+
+        if(num % 2 == 0){
+            return false;
+        }
+
+        for(int i = 3; i < Math.sqrt(num); i += 2){
+            if(num % 3 == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    private static boolean isSumEven(int num){
+        int sum = 0;
+        int temp = num;
+        while(temp > 10){
+             sum += temp % 10;
+            temp = temp / 10;
+           
+        }
+        sum += temp;
+        return sum % 2 == 0;
+    }
+}
